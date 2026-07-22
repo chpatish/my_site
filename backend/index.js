5c98c7ea362b399b9a0a3db7c8eaad93b6913ca0
chpatish <patimatcamkurgadzieva66861@gmail.com>
2026-07-23 01:52:49 +0300

    fix port variable

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,8 @@ const path = require("path");
 const { connectDB, pool } = require("./db"); 
 
 const app = express(); 
-const PORT = 5000; 
+const PORT = process.env.PORT || 5000;
+
 
 // 1. Настройки сервера
 app.use(express.json()); 
